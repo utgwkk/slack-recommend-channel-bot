53c0683a4e86ea56e92c53e42c447d57153a7612
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-08 17:28:11 +0900

    gofmt

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"os"
-	"log"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 
-	"github.com/nlopes/slack"
 	"github.com/joho/godotenv"
+	"github.com/nlopes/slack"
 )
 
 func postMessage(api *slack.Client, channelID, text string) (string, string, error) {
@@ -23,17 +23,17 @@ func postMessage(api *slack.Client, channelID, text string) (string, string, err
 	return api.PostMessage(channelID, msgOption)
 }
 
-func main () {
+func main() {
 	err := godotenv.Load()
 	if err != nil {
-	  log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
 
 	apiKey, ok := os.LookupEnv("SLACK_TOKEN")
 	if !ok {
 		log.Fatalln("API key not set in SLACK_TOKEN")
 	}
-	postChannelID, ok :=os.LookupEnv("POST_CHANNEL_ID")
+	postChannelID, ok := os.LookupEnv("POST_CHANNEL_ID")
 	if !ok {
 		log.Fatalln("Post destination channel ID not set in POST_CHANNEL_ID")
 	}
@@ -54,6 +54,7 @@ func main () {
 	rand.Seed(time.Now().UnixNano())
 	index := rand.Intn(len(channelNames))
 	todaysRecommendChannel := channelNames[index]
-	text := fmt.Sprintf("今日のおすすめチャンネルは……これ！！！！👉👉👉👉👉 %s 👈👈👈👈👈\n", todaysRecommendChannel)
+	text := fmt.Sprintf("今日のおすすめチャンネルは……これ！！！！👉👉👉👉👉 %s 👈👈👈👈👈", todaysRecommendChannel)
+	log.Printf("post %s to %s", text, postChannelID)
 	postMessage(api, postChannelID, text)
 }
